b34c63a3caa24d17c6b47f8d4e874b1c9f4cd49d
Ali Reza Delbari <Alirezadelbari20@yahoo.com>
2025-06-18 11:07:26 +0330

    asasi

--- test1.js
+++ test1.js
@@ -1,10 +1,15 @@
+
+// x=[MerchantID][SocketID][CoocieID]
 const x = 
     {
+      // Merchant ID
   '672762cd980313553afed313': {
+        // SocketID
     '8EYMUMwTOkOrk1-kAAAQ': {
       userData: [Object],
       name: 'ali',
       id: '8EYMUMwTOkOrk1-kAAAQ',
+      // CookieID
       cookieId: 'esSIATIIBidq2zTHAAAN',
       merchantId: new ObjectId('672762cd980313553afed313'),
       targetOperator: null,
